022f729c982f9d4deb008c5886677351692d8b60
Arthur Rodrigues <arthur.rgs03@gmail.com>
2024-06-06 01:15:40 -0300

    proposta Final

--- main.py
+++ main.py
@@ -1,42 +1,153 @@
-def ler_nivel_agua(index):
-    nivel_agua = [15, 30, 85, 70, 55, 45, 20, 95, 50, 40]
-    return nivel_agua[index]
+opc_binario = ["S", "N"]
+opc_pesquisas = [
+  {
+    "nome": "Fossa de Leviathan",
+    "pH": 6.5,
+    "movimento_peixes": 42,
+    "temperatura": 25
+  },
+  {
+    "nome": "Jardim de Poseidon",
+    "pH": 7.2,
+    "movimento_peixes": 38,
+    "temperatura": 28
+  },
+  {
+    "nome": "Caldeira de Vulcano",
+    "pH": 5.8,
+    "movimento_peixes": 23,
+    "temperatura": 22
+  },
+  {
+    "nome": "Ilha da Aurora",
+    "pH": 6.7,
+    "movimento_peixes": 56,
+    "temperatura": 26
+  },
+  {
+    "nome": "Mar de Sirenas",
+    "pH": 8.0,
+    "movimento_peixes": 64,
+    "temperatura": 21
+  },
+  {
+    "nome": "Baía dos Kraken",
+    "pH": 5.5,
+    "movimento_peixes": 31,
+    "temperatura": 30
+  },
+  {
+    "nome": "Triângulo das Bermudas",
+    "pH": 6.1,
+    "movimento_peixes": 45,
+    "temperatura": 24
+  },
+  {
+    "nome": "Mar de Netuno",
+    "pH": 7.4,
+    "movimento_peixes": 29,
+    "temperatura": 27
+  },
+  {
+    "nome": "Arquipélago dos Corais",
+    "pH": 5.9,
+    "movimento_peixes": 52,
+    "temperatura": 23
+  },
+  {
+    "nome": "Estreito de Andrômeda",
+    "pH": 6.3,
+    "movimento_peixes": 60,
+    "temperatura": 25
+  }
+]
 
-def ler_ph(index):
-    valor_do_ph = [6.5, 7.0, 8.5, 7.2, 6.8, 7.5, 6.0, 8.0, 7.1, 6.9]
-    return valor_do_ph[index]
+def validadorBinario(selecao, mensagem):
+    validador = selecao
+    while not validador.upper() in opc_binario:
+        validador = input(mensagem)
+    return validador.upper()
 
-def conta_peixes(index):
-    contagem_peixes = [2, 5, 8, 6, 4, 3, 1, 9, 7, 10]
-    return contagem_peixes[index]
+def entrar():
+    selecao = input("Você deseja acessar o sistema? (S/N): ")
+    binario = validadorBinario(selecao, "Opção inválida! Por favor, digite 'S' para sim ou 'N' para não: ")
 
-for i in range(10):
-    nivel_agua = ler_nivel_agua(i)
-    valor_do_ph = ler_ph(i)
-    contagem_peixes = conta_peixes(i)
+    if binario in opc_binario:
+        if binario == "S":
+            print("Acessando o sistema...\n")
+            inicializador()
+        else:
+            print("Obrigado por acessar a plataforma DMO!")
 
-    if nivel_agua < 20:
-        status_da_agua = "Baixo"
-    elif 20 <= nivel_agua <= 80:
-        status_da_agua = "Normal"
+def validadorNumerico(selecao, mensagem):
+    validador = selecao
+    while not validador.isnumeric():
+        validador = input(mensagem)
+    validador = int(validador)
+    return validador
+
+def validadorSelecao(numero, mensagem):
+    validador = numero
+    while not 0 <= validador <= len(opc_pesquisas) - 1:
+        validador = input(mensagem)
+        validacao = validadorNumerico(validador, "Opção inválida! Por favor, digite um número: ")
+        validador = validacao
+    validador = int(validador)
+    return validador
+
+def status(ph, movimento_peixes, temperatura):
+    if ph < 7:
+        status_do_ph = "Ácida"
+    elif ph == 7:
+        status_do_ph = "Neutra"
     else:
-        status_da_agua = "Alto"
+        status_do_ph = "Alcalina"
 
-    if valor_do_ph < 7:
-        status_do_ph = "Ácido"
-    elif valor_do_ph == 7:
-        status_do_ph = "Neutro"
-    else:
-        status_do_ph = "Alcalino"
-
-    if contagem_peixes < 3:
+    if movimento_peixes < 3:
         status_dos_peixes = "Poucos Peixes"
-    elif 3 <= contagem_peixes <= 7:
-        status_dos_peixes = "Quantidade Adequada"
+    elif 3 <= movimento_peixes <= 7:
+        status_dos_peixes = "Quantidade Razóavel"
     else:
-        status_dos_peixes = "Muitos Peixes"
+        status_dos_peixes = "Quantidade Saudável"
 
-    print(f"Nível de Água: {nivel_agua:.2f}% - Status: {nivel_agua}")
-    print(f"pH da Água: {valor_do_ph:.2f} - Status: {status_do_ph}")
-    print(f"Quantidade de Peixes: {contagem_peixes} - Status: {status_dos_peixes}")
-    print("-" * 40)
+    if temperatura < 25:
+        status_da_temperatura = "Baixa"
+    elif 25 <= temperatura <= 28:
+        status_da_temperatura = "Normal"
+    else:
+        status_da_temperatura = "Alta"
+
+    return {"ph": status_do_ph, "peixes": status_dos_peixes, "temperatura": status_da_temperatura}
+
+def inicializador():
+    print("Que bom que está aqui. Vamos começar!\n")
+    while True:
+        contagem = -1
+        string = "\nPara selecionar uma opção, digite o número correspondente:\n"
+        for pesquisa in opc_pesquisas:
+            contagem += 1
+            string = string + f"{contagem}) {pesquisa['nome']} \n"
+        string = string + "->"
+        selecao = input(string)
+        numeroSelec = validadorNumerico(selecao, "Opção inválida! Por favor, digite um número: ")
+        numeroFinal = validadorSelecao(numeroSelec, f"Opção inválida! Por favor, digite um número de 0 a {len(opc_pesquisas) - 1}: ")
+
+        statusPesquisa = status(opc_pesquisas[numeroFinal]['pH'], opc_pesquisas[numeroFinal]['movimento_peixes'], opc_pesquisas[numeroFinal]['temperatura'])
+
+        print("\nSeleção realizada com sucesso!\n"
+              "Aqui estão os dados da sua pesquisa:\n")
+
+        print(f"Nome da Pesquisa: {opc_pesquisas[numeroFinal]['nome']}\n"
+              f"pH da Água: {opc_pesquisas[numeroFinal]['pH']} - {statusPesquisa['ph']}\n"
+              f"Movimentação dos Peixes: {opc_pesquisas[numeroFinal]['movimento_peixes']} - {statusPesquisa['peixes']}\n"
+              f"Temperatura da Água: {opc_pesquisas[numeroFinal]['temperatura']}°C - {statusPesquisa['temperatura']}\n")
+
+        continuar = input("Deseja pesquisar mais? (S/N): ")
+        binario = validadorBinario(continuar, "Opção inválida! Por favor, digite 'S' para sim ou 'N' para não: ")
+
+        if binario == "N":
+            print("Obrigado por acessar a plataforma DMO!")
+            break
+
+print("Seja bem vindo à plataforma DMO!")
+entrar()
